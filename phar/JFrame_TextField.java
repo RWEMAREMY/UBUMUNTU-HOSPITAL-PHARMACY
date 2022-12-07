e1ee947e7372c1f3ada390bc4fea674c78e82c48
RWEMAREMY <120011390+RWEMAREMY@users.noreply.github.com>
2022-12-07 12:02:19 +0200

    Add files via upload

---
+++ phar/JFrame_TextField.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ubumuntu.hosp.phar;
+
+/**
+ *
+ * @author REMY
+ */
+class JFrame_TextField {
+
+    void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setLocationRelativeTo(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setDefaultCloseOperation(int DISPOSE_ON_CLOSE) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
